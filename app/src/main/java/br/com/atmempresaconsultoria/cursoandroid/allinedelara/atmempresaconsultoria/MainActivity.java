05847379714bc4d8913ad327683795d647b5a253
Alline de Lara Haiduski <allinelara@gmail.com>
2017-04-23 21:18:15 -0300

    adicionando arquivos do app ATM Empresa de Consultoria

---
+++ app/src/main/java/br/com/atmempresaconsultoria/cursoandroid/allinedelara/atmempresaconsultoria/MainActivity.java
@@ -0,0 +1,53 @@
+package br.com.atmempresaconsultoria.cursoandroid.allinedelara.atmempresaconsultoria;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView botaoEmpresa;
+    private ImageView botaoContato;
+    private ImageView botãoClientes;
+    private ImageView botaoServicos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoEmpresa = (ImageView) findViewById(R.id.empresaID);
+        botaoEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
+
+        botaoServicos = (ImageView) findViewById(R.id.servicosID);
+        botaoServicos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ServicoActivity.class));
+            }
+        });
+
+        botãoClientes = (ImageView) findViewById(R.id.clientesID);
+        botãoClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ClienteActivity.class));
+            }
+        });
+
+        botaoContato = (ImageView) findViewById(R.id.contatoID);
+        botaoContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ContatoActivity.class));
+            }
+        });
+    }
+}
